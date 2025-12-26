8c853bdfbe58c5ea20fe276a36b571e1ecee9419
Einav Haimovich <einav@haimol.net>
2025-12-26 17:52:41 +0200

    sorting and filtering added

---
+++ rest-api-course-final-master/5.AdvancedFeatures/Movies.Application/Models/GetAllMoviesOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Movies.Application.Models
+{
+    public class GetAllMoviesOptions
+    {
+        public required string? Title { get; set; }
+        public required int? Year { get; set; }
+        public string? SortField { get; set; }
+        public SortOrder? SortOrder { get; set; }
+        public Guid? userId { get; set; }
+    }
+
+    public enum SortOrder
+    {
+        Unsorted,
+        Ascending,
+        Descending
+    }
+}
---
+++ rest-api-course-final-master/5.AdvancedFeatures/Movies.Application/Validators/GetAllMoviesOptionsValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Movies.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Application.Validators
+{
+    public class GetAllMoviesOptionsValidator : AbstractValidator<GetAllMoviesOptions>
+    {
+        private static readonly string[] AcceptableSortFields =
+        {
+            "title", "year"
+        };
+
+        public GetAllMoviesOptionsValidator()
+        {
+            RuleFor(x => x.Year)
+                .LessThanOrEqualTo(DateTime.UtcNow.Year);
+            RuleFor(x => x.SortField).Must(x => x is null || AcceptableSortFields.Contains(x, StringComparer.OrdinalIgnoreCase))
+                .WithMessage("You can only sort by 'title' or 'year' ");
+        }
+    }
+}
